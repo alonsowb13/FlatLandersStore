f97894377abc7088bb8a0aed5fc5dc548f757039
alonsowb13 <alonsosalcido0318@gmail.com>
2017-04-25 08:46:02 -0600

    Adding an array to scope

--- flatStore/js/app.js
+++ flatStore/js/app.js
@@ -10,8 +10,32 @@
 		soldOut: true
 	}
 
+	var gems = [
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
+		canPurchase: false,
+		soldOut: false
+	},
+	{
+		name: 'Ruby',
+		price: 3.95,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
+		canPurchase: false,
+		soldOut: false
+	},
+	{
+		name: 'Safiro',
+		price: 6.95,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
+		canPurchase: false,
+		soldOut: false
+	}
+	];
+
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
 })();
