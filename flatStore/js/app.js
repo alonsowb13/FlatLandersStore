52da02709cb0f278a4f346b18fd307b861b5b680
alonsowb13 <alonsosalcido0318@gmail.com>
2017-04-25 08:35:17 -0600

    Product gem in the DOM

--- flatStore/js/app.js
+++ flatStore/js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('store', []);
+(function(){
+
+	var app = angular.module('store', []);
+
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
+		canPurchase: false,
+		soldOut: true
+	}
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+})();
