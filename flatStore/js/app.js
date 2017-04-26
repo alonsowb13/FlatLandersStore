16e3569d796c0dddb7ff43aef599225ab34543ca
alonsowb13 <alonsosalcido0318@gmail.com>
2017-04-26 08:59:18 -0600

    Adding images and panels to the view, incomplete controller behavoir

--- flatStore/js/app.js
+++ flatStore/js/app.js
@@ -15,27 +15,47 @@
 		name: 'Dodecahedron',
 		price: 2.95,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
-		canPurchase: false,
-		soldOut: false
+		images:[
+			{
+				full: 'public/img/gem-01.gif',
+				thumb: 'public/img/gem-04.gif'
+			}
+		]
 	},
 	{
 		name: 'Ruby',
 		price: 3.95,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
-		canPurchase: false,
-		soldOut: false
+		images:[
+			{
+				full: 'public/img/gem-02.gif',
+				thumb: 'public/img/gem-05.gif'
+			}
+		]
 	},
 	{
 		name: 'Safiro',
 		price: 6.95,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, numquam ea! Sequi, nemo maiores culpa, inventore temporibus',
-		canPurchase: false,
-		soldOut: false
+		images:[
+			{
+				full: 'public/img/gem-03.gif',
+				thumb: 'public/img/gem-06.gif'
+			}
+		]
 	}
 	];
 
 	app.controller('StoreController', function(){
 		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		}
 	});
 
 })();
